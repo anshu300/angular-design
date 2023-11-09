1321ac544c393684d5a287dfc3a8fbf87299e710
anshuverma-sdm <anshu.verma@smartdatainc.net>
2023-11-09 20:11:14 +0530

    done

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,17 @@
 import { Routes } from '@angular/router';
-
-export const routes: Routes = [];
+import { HomePageComponent } from './chat/home-page/home-page.component';
+import { LoginComponent } from './chat/login/login.component';
+import { RegisterComponent } from './chat/register/register.component';
+import { HeaderComponent } from './chat_layout/header/header.component';
+import { Component } from '@angular/core';
+export const routes: Routes = [
+    { path: 'chat/login', component: LoginComponent },
+    { path: 'chat/register', component: RegisterComponent },
+    {
+        path: '',
+        component: HeaderComponent,
+        children: [
+            { path: 'chat/home-page', component: HomePageComponent },
+        ]
+      },
+];
